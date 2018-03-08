bee49be1abfc09a07e2596693c5051ca47cef038
mattperkins <github@mattperkins.com>
2018-03-08 17:13:17 +0000

    add content to pages

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,5 +1,17 @@
 import React from 'react'
 
-export const HomePage = () => (<p>HomePage</p>)
-export const AboutPage = () => (<p>AboutPage</p>)
+export const HomePage = () => (
+<div>
+<h1>HomePage</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores possimus aliquid illum a, eveniet repudiandae velit dolor quae autem iusto eius obcaecati. Velit iste, sed ipsam error fuga cupiditate quisquam.</p>
+</div>
+)
+
+
+export const AboutPage = () => (
+<div>
+<h1>AboutPage</h1>
+<p>Asperiores possimus aliquid illum a, eveniet repudiandae velit dolor quae autem iusto eius obcaecati. Velit iste, sed ipsam error fuga cupiditate quisquam.Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+</div>
+)
 
