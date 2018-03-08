333f6da95f06b3c70749c2b6bf74a9de8f2d0fd1
mattperkins <github@mattperkins.com>
2018-03-08 17:36:36 +0000

    correct styling

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -3,14 +3,14 @@ import { Wrapper } from '../styles'
 
 export const HomePage = () => (
 <Wrapper>
-<h1>HomePage</h1>
+<h1>Home Page</h1>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores possimus aliquid illum a, eveniet repudiandae velit dolor quae autem iusto eius obcaecati. Velit iste, sed ipsam error fuga cupiditate quisquam.</p>
 </Wrapper>
 )
 
 export const AboutPage = () => (
 <Wrapper>
-<h1>AboutPage</h1>
+<h1>About Page</h1>
 <p>Asperiores possimus aliquid illum a, eveniet repudiandae velit dolor quae autem iusto eius obcaecati. Velit iste, sed ipsam error fuga cupiditate quisquam.Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 </Wrapper>
 )
