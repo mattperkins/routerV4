3450d5a15fc8c1ab9ce0abb21a7c82e1a3ee644d
mattperkins <github@mattperkins.com>
2018-12-31 01:17:19 +0000

    refactor > number of paragraphs + lodash

--- src/pages/Paras.js
+++ src/pages/Paras.js
@@ -5,22 +5,21 @@
 import React from 'react'
 import { flatten, times } from 'lodash'
 
-export default class Paras extends React.Component{
-    render() {
+export default class Paras extends React.Component {
+  render () {
+    const NUMBER_OF_PARAGRAPHS = 15
+    const paragraphs = flatten(times(NUMBER_OF_PARAGRAPHS, (i) => {
+      return (
+        <p key={i}>
+     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque nemo, inventore vitae, sint quibusdam nobis et enim aliquam veniam nam aut dolore hic beatae fugit ducimus possimus quos, nihil quisquam.
+        </p>
+      )
+    }))
 
-        const NUMBER_OF_PARAGRAPHS = 15
-        const paragraphs = flatten(times(NUMBER_OF_PARAGRAPHS, (i) => {
-            return (
-                <p key={i}>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque nemo, inventore vitae, sint quibusdam nobis et enim aliquam veniam nam aut dolore hic beatae fugit ducimus possimus quos, nihil quisquam.
-                </p>
-             )
-        }))
-
-        return(
-            <div style={{columns: 2, columnGap: '3rem'}}>
-            { paragraphs.map(paragraph => paragraph) }
-            </div>
-        )
-    }
+    return (
+      <div style={{ columns: 2, columnGap: '3rem' }}>
+        {paragraphs.map(paragraph => paragraph)}
+      </div>
+    )
+  }
 }
